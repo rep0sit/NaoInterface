907c0fffabfac243c81ff56c295b3f83edd5245a
Etienne Onasch <etienneonasch@web.de>
2017-12-11 17:32:16 +0100

    Konstanten in speech.utils.Words erstellt

--- src/speech/utils/Words.java
+++ src/speech/utils/Words.java
@@ -7,5 +7,16 @@ final class Words {
 	private Words() {}
 	
 	public static final List<String> DIGITS = Arrays.asList("One","Two","Three",
-																"Four", "Five", "Six");
+																"Four", "Five", "Six",
+																"Seven", "Eight", "Nine");
+	
+	public static final List<String> DIGITS_NATO = Arrays.asList("Won","Too","Free",
+			"Four", "Five", "Six",
+			"Seven", "Eight", "Niner");
+	
+	
+	
+	
+	
+	
 }
